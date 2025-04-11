599e243d7bb85711d7e43acf9b06ebbfbdb8fa84
Nexus-FayazKhan <fayaz.khanxid411@gmail.com>
2025-04-11 12:15:34 +0530

    Update GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001', // Updated to port 5001
+        target: 'https://nexus-fayazkhan.github.io/', // Updated to port 5001
         changeOrigin: true,
         secure: false,
       },
